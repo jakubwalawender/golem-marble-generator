ad761c16b708567b0a7e428d82157bcdc2c69996
Jakub Walawender <jakub.e.walawender@gmail.com>
2022-08-21 22:07:46 +0200

    Change default page title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 import { Synagogue } from "./components/Synagogue";
-import { Marble } from "./components/Marble";
 
 import "./App.css";
 
-const App = () => (
-  <div className="holder">
-    <div className="title">Golem Marble Generator</div>
-    <Synagogue />
+const App = () => {
+  document.title = "Golem Marble Generator";
+  return (
+    <div className="holder">
+      <div className="title">Golem Marble Generator</div>
+      <Synagogue />
     </div>
-);
+  );
+};
 
 export default App;
